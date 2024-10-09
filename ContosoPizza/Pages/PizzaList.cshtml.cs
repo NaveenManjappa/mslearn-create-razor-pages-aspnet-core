3f904a718969e503defbbd3e416fa9b9dce5d117
Naveen Manjappa <NManjappa@manh.com>
2024-10-09 16:29:14 +0100

    Implementing the Pizza list page

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -1,3 +1,5 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,16 @@ namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
     {
+        private readonly PizzaService _service;
+        public IList<Pizza> PizzaList { get; set;}=default!;
+
+        public PizzaListModel(PizzaService pizzaService){
+            this._service=pizzaService;
+        }
+
         public void OnGet()
         {
+            PizzaList = this._service.GetPizzas();
         }
     }
 }
